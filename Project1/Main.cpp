064560dd5d7af367699966f094e5c9c7c5a6717f
ITSTEP\st <st>
2024-03-21 17:56:59 +0300

    .

--- Project1/Main.cpp
+++ Project1/Main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
+#define SIZE 10
 
 using namespace std;
 
+int sum(int, int, int);
+
 int main() {
+	int x = 7;
+	int y = 10;
+	int z = 20;
 
-
+	cout << sum(x, y, z) << endl;
+	
 	return 0;
 }
